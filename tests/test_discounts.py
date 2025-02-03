05e343e8dac8a27efd72c88071961f61bb30c473
Joe Bullard <joekbullard@gmail.com>
2025-02-03 10:56:00 +0000

    add test for calculating total to pay after savings

--- tests/test_discounts.py
+++ tests/test_discounts.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 import unittest
 from test_data import test_items, test_offers
 from discounts import OfferCalculator
+from shopping_basket import ShoppingBasket
 
 
 class TestOfferCalculator(unittest.TestCase):
@@ -17,6 +18,15 @@ class TestOfferCalculator(unittest.TestCase):
         )
         total_savings = sum([discount.discount_amount for discount in discounts])
         self.assertEqual(total_savings, Decimal("-0.90"))
+
+    def test_total_payment(self):
+        shopping_basket = ShoppingBasket(self.test_items)
+        subtotal = shopping_basket.subtotal()
+        offer_caulculator = OfferCalculator(offers=self.test_offers)
+        discounts = offer_caulculator.calculate_offers(shopping_basket.items)
+        total_savings = sum([discount.discount_amount for discount in discounts])
+        total_to_pay = subtotal + total_savings
+        self.assertEqual(total_to_pay, Decimal('2.40'))
 
 if __name__ == "__main__":
     unittest.main()
