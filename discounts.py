7c54224683f6a3210bd5dc757f74d9bc620bdab4
Joe Bullard <joekbullard@gmail.com>
2025-02-03 08:00:56 +0000

    add calculate_offer_discount method to OfferCalculator

--- discounts.py
+++ discounts.py
@@ -19,3 +19,17 @@ class Offer:
 @dataclass
 class OfferCalculator:
     offers: List[Offer]
+
+    def calculate_offer_discount(self, basket_items: List[BasketItem]) -> Decimal:
+        total_savings = 0
+        for offer in self.offers:
+            qualifying_items = [item for item in basket_items if item.name in offer.qualifying_items]
+
+            if len(qualifying_items) >= offer.qualifying_quantity:
+                # use floor division to return number of discounts to apply
+                number_discounts = len(qualifying_items) // offer.qualifying_quantity
+
+                offer_savings = number_discounts * -offer.discount_amount
+                total_savings += offer_savings
+        
+        return total_savings
