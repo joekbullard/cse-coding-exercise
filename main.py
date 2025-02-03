edf4201578fdeae1b16d37288121b1c988aa682e
Joe Bullard <joekbullard@gmail.com>
2025-02-03 08:44:10 +0000

    add example items and offers to main.py

---
+++ main.py
@@ -0,0 +1,69 @@
+from shopping_basket import BasketItem, ShoppingBasket, PriceType
+from discounts import Offer, OfferCalculator
+from decimal import Decimal
+
+'''
+Example script demonstrating shopping basket calculator
+'''
+
+shopping_items = [
+    BasketItem(
+        name="Beans", 
+        unit_price=Decimal("0.50"), 
+        price_type=PriceType.FIXED
+    ),
+    BasketItem(
+        name="Beans", 
+        unit_price=Decimal("0.50"), 
+        price_type=PriceType.FIXED
+    ),
+    BasketItem(
+        name="Beans", 
+        unit_price=Decimal("0.50"), 
+        price_type=PriceType.FIXED
+    ),
+    BasketItem(
+        name="Beans", 
+        unit_price=Decimal("0.50"), 
+        price_type=PriceType.FIXED
+    ),
+    BasketItem(
+        name="Coke",
+        unit_price=Decimal("0.70"),
+        price_type=PriceType.FIXED,
+    ),
+    BasketItem(
+        name="Coke",
+        unit_price=Decimal("0.70"),
+        price_type=PriceType.FIXED,
+    ),
+    BasketItem(
+        name="Coke",
+        unit_price=Decimal("0.70"),
+        price_type=PriceType.FIXED,
+    ),
+    BasketItem(
+        name="Onions",
+        unit_price=Decimal("0.29"),
+        weight=Decimal("0.400"),
+        price_type=PriceType.WEIGHED,
+    ),
+]
+
+
+offers = [
+    Offer(
+        name='Coke two for £1',
+        qualifying_items=['Coke'],
+        qualifying_quantity=2, 
+        discount_amount=Decimal('0.40')
+    ),
+    Offer(
+        name='Beans 3 for 2',
+        qualifying_items=['Beans'],
+        qualifying_quantity=3, 
+        discount_amount=Decimal('0.50')
+    ),
+]
+
+
