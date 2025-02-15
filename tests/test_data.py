3ae31941e62bdce110cc69f48572c8991d356ade
Joe Bullard <joekbullard@gmail.com>
2025-02-15 19:24:56 +0000

    Include FIXED_BUNDLE offers and items

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,5 +1,5 @@
 from shopping_basket import BasketItem, PriceType
-from discounts import Offer
+from discounts import Offer, OfferType
 from decimal import Decimal
 
 test_offers = [
@@ -7,14 +7,23 @@ test_offers = [
         name='Coke two for £1',
         qualifying_items=['Coke'],
         qualifying_quantity=2, 
+        offer_type=OfferType.FIXED_ITEM,
         discount_amount=Decimal('0.40')
     ),
     Offer(
         name='Beans 3 for 2',
         qualifying_items=['Beans'],
         qualifying_quantity=3, 
+        offer_type=OfferType.FIXED_ITEM,
         discount_amount=Decimal('0.50')
     ),
+    Offer(
+        name='3 Ales for £6',
+        qualifying_items=['NEIPA', 'IPA', 'Golden Ale', 'Stout'],
+        qualifying_quantity=3,
+        offer_type=OfferType.FIXED_BUNDLE,
+        discount_amount=Decimal('6.00')
+    )
 ]
 
 test_items = [
@@ -48,5 +57,31 @@ test_items = [
         unit_price=Decimal("1.99"),
         weight=Decimal("0.200"),
         price_type=PriceType.WEIGHED,
+    ),
+    BasketItem(
+        name="IPA",
+        unit_price=Decimal("2.99"),
+        price_type=PriceType.FIXED,
+    ),
+    BasketItem(
+        name="NEIPA",
+        unit_price=Decimal("2.39"),
+        price_type=PriceType.FIXED,
+    ),
+    BasketItem(
+        name="Stout",
+        unit_price=Decimal("2.29"),
+        weight=Decimal("0.200"),
+        price_type=PriceType.FIXED,
+    ),
+    BasketItem(
+        name="IPA",
+        unit_price=Decimal("2.99"),
+        price_type=PriceType.FIXED,
+    ),
+    BasketItem(
+        name="Stout",
+        unit_price=Decimal("2.29"),
+        price_type=PriceType.FIXED,
     ),
 ]
