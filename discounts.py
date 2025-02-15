2a727eeda49d3d9c87f37cc6b1df369f17bbcd6f
Joe Bullard <joekbullard@gmail.com>
2025-02-15 22:15:44 +0000

    Refactors FIXED_BUNDLE to use list comprehension

--- discounts.py
+++ discounts.py
@@ -19,6 +19,7 @@ class Offer:
     qualifying_items: List[str]
     qualifying_quantity: int
     offer_type: OfferType
+    # this doesn't seem right when using for FIXED_BUNDLE
     discount_amount: Decimal = None
 
     def __str__(self):
@@ -57,8 +58,6 @@ class OfferCalculator:
             qualifying_items = [item for item in items if item.name in offer.qualifying_items]
             if len(qualifying_items) >= offer.qualifying_quantity:
 
-                # use floor division to return number of discounts to apply
-                # this can go outside if statement as it applies to both types
                 number_discounts = len(qualifying_items) // offer.qualifying_quantity
 
                 if offer.offer_type == OfferType.FIXED_ITEM:
@@ -66,15 +65,7 @@ class OfferCalculator:
 
                 elif offer.offer_type == OfferType.FIXED_BUNDLE:
                     total_items_in_bundle = number_discounts * offer.qualifying_quantity
-                    offer_item_subtotal = 0
-                    for item in qualifying_items[:total_items_in_bundle]:
-                        offer_item_subtotal += item.total_price
-                    offer_savings =  offer.discount_amount - offer_item_subtotal
+                    offer_item_subtotal = sum([item.total_price for item in qualifying_items[:total_items_in_bundle]])
+                    offer_savings =  (offer.discount_amount * number_discounts) - offer_item_subtotal
 
                 yield OfferDiscount(name=offer.name, discount_amount=offer_savings)
-
-                    # cheapest_items = qualifying_items[:offer.qualifying_quantity]
-                    # for _ in range(number_discounts):
-                    #     total_price = sum([item.price for item in cheapest_items]) 
-                    #     saving =  offer.discount_amount - total_price
-                    # yield OfferDiscount(name=offer.name, discount_amount=saving)
--- main.py
+++ main.py
@@ -1,62 +1,103 @@
 from shopping_basket import BasketItem, ShoppingBasket, PriceType
-from discounts import Offer, OfferCalculator
+from discounts import Offer, OfferCalculator, OfferType
 from decimal import Decimal
 
 '''
 Example script demonstrating shopping basket calculator
 '''
 
-shopping_items = [
-    BasketItem(
-        name="Beans", 
-        unit_price=Decimal("0.50")
-    ),
-    BasketItem(
-        name="Beans", 
-        unit_price=Decimal("0.50"), 
-    ),
-    BasketItem(
-        name="Beans", 
-        unit_price=Decimal("0.50"), 
-    ),
-    BasketItem(
-        name="Beans", 
-        unit_price=Decimal("0.50"), 
-    ),
-    BasketItem(
-        name="Coke",
-        unit_price=Decimal("0.70"),
-    ),
-    BasketItem(
-        name="Coke",
-        unit_price=Decimal("0.70"),
-    ),
-    BasketItem(
-        name="Coke",
-        unit_price=Decimal("0.70"),
-    ),
-    BasketItem(
-        name="Onions",
-        unit_price=Decimal("0.29"),
-        weight=Decimal("0.400"),
-        price_type=PriceType.WEIGHED,
-    ),
-]
-
-
 offers = [
     Offer(
         name='Coke two for £1',
         qualifying_items=['Coke'],
         qualifying_quantity=2, 
+        offer_type=OfferType.FIXED_ITEM,
         discount_amount=Decimal('0.40')
     ),
     Offer(
         name='Beans 3 for 2',
         qualifying_items=['Beans'],
         qualifying_quantity=3, 
+        offer_type=OfferType.FIXED_ITEM,
         discount_amount=Decimal('0.50')
     ),
+    Offer(
+        name='3 Ales for £6',
+        qualifying_items=['NEIPA', 'IPA', 'Golden Ale', 'Stout'],
+        qualifying_quantity=3,
+        offer_type=OfferType.FIXED_BUNDLE,
+        discount_amount=Decimal('6.00')
+    )
+]
+
+shopping_items = [
+    BasketItem(
+        name="Beans", 
+        unit_price=Decimal("0.50"), 
+        price_type=PriceType.FIXED
+    ),
+    BasketItem(
+        name="Beans", 
+        unit_price=Decimal("0.50"), 
+        price_type=PriceType.FIXED
+    ),
+    BasketItem(
+        name="Beans", 
+        unit_price=Decimal("0.50"), 
+        price_type=PriceType.FIXED
+    ),
+    BasketItem(
+        name="Coke",
+        unit_price=Decimal("0.70"),
+        price_type=PriceType.FIXED,
+    ),
+    BasketItem(
+        name="Coke",
+        unit_price=Decimal("0.70"),
+        price_type=PriceType.FIXED,
+    ),
+    BasketItem(
+        name="Oranges",
+        unit_price=Decimal("1.99"),
+        weight=Decimal("0.200"),
+        price_type=PriceType.WEIGHED,
+    ),
+    BasketItem(
+        name="IPA",
+        unit_price=Decimal("2.99"),
+        price_type=PriceType.FIXED,
+    ),
+    BasketItem(
+        name="NEIPA",
+        unit_price=Decimal("2.39"),
+        price_type=PriceType.FIXED,
+    ),
+    BasketItem(
+        name="Stout",
+        unit_price=Decimal("2.29"),
+        weight=Decimal("0.200"),
+        price_type=PriceType.FIXED,
+    ),
+    BasketItem(
+        name="IPA",
+        unit_price=Decimal("2.99"),
+        price_type=PriceType.FIXED,
+    ),
+    BasketItem(
+        name="Stout",
+        unit_price=Decimal("2.29"),
+        price_type=PriceType.FIXED,
+    ),
+    BasketItem(
+        name="IPA",
+        unit_price=Decimal("2.99"),
+        price_type=PriceType.FIXED,
+    ),
+    BasketItem(
+        name="Stout",
+        unit_price=Decimal("2.29"),
+        price_type=PriceType.FIXED,
+    )
 ]
 
 
