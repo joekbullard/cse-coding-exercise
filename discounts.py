00f4b0c4134a191c692ae3019500430b88a2b838
Joe Bullard <joekbullard@gmail.com>
2025-02-15 19:05:23 +0000

    Implements FIXED_BUNDLE discount

--- discounts.py
+++ discounts.py
@@ -5,7 +5,7 @@ from shopping_basket import BasketItem
 from enum import Enum, auto
 
 
-class DiscountType(Enum):
+class OfferType(Enum):
     FIXED_BUNDLE = auto()
     FIXED_ITEM = auto()
     # could also have %age or threshold e.g.
@@ -18,14 +18,14 @@ class Offer:
     name: str
     qualifying_items: List[str]
     qualifying_quantity: int
-    discount_type: DiscountType
+    offer_type: OfferType
     discount_amount: Decimal
 
     def __str__(self):
         return f"{self.name}"
     
 
-""" For instances where multiple BasketItems you could have like this:
+""" For instances where multiple BasketItem in an offer you could have like this:
 offer = Offer("3 Ales for £6", [golden_ale, ipa, neipa, stout], 3, DiscountType.FIXED_BUNDLE, 6.00)
 """
 
@@ -46,15 +46,36 @@ class OfferCalculator:
     offers: List[Offer]
 
     def calculate_offers(self, items: List[BasketItem]) -> List[OfferDiscount]:
-        return list(self._calculate_offer_discount(items))
+        # Probably fine, although in cases where there an 
+        sorted_items = sorted(items, key=lambda item: item.total_price)
+        return list(self._calculate_offer_discount(sorted_items))
 
 
     def _calculate_offer_discount(self, items: List[BasketItem]) -> Iterator[OfferDiscount]:
         for offer in self.offers:
-            qualifying_items = [item for item in items if item.name in offer.qualifying_items]
 
+            qualifying_items = [item for item in items if item.name in offer.qualifying_items]
             if len(qualifying_items) >= offer.qualifying_quantity:
+
                 # use floor division to return number of discounts to apply
+                # this can go outside if statement as it applies to both types
                 number_discounts = len(qualifying_items) // offer.qualifying_quantity
-                offer_savings = number_discounts * -offer.discount_amount
+
+                if offer.offer_type == OfferType.FIXED_ITEM:
+                    offer_savings = number_discounts * -offer.discount_amount
+
+                elif offer.offer_type == OfferType.FIXED_BUNDLE:
+                    total_items_in_bundle = number_discounts * offer.qualifying_quantity
+                    offer_item_subtotal = 0
+                    for _ in range(total_items_in_bundle):
+                        item = next(qualifying_items)
+                        offer_item_subtotal += item.total_price
+                    offer_savings =  offer.discount_amount - offer_item_subtotal
+
                 yield OfferDiscount(name=offer.name, discount_amount=offer_savings)
+
+                    # cheapest_items = qualifying_items[:offer.qualifying_quantity]
+                    # for _ in range(number_discounts):
+                    #     total_price = sum([item.price for item in cheapest_items]) 
+                    #     saving =  offer.discount_amount - total_price
+                    # yield OfferDiscount(name=offer.name, discount_amount=saving)
